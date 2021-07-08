3da1079617b2bda05ebe805ddef496d051c4414d
Vladislav Biketov <vladtheartist@gmail.com>
2021-07-08 22:52:42 +0500

    [new]

--- part_2/lesson_1/1.4.0.rb
+++ part_2/lesson_1/1.4.0.rb
@@ -5,6 +5,19 @@ require 'date'
 past = Time.local(2019, 10, 0o7)
 now = Time.local(2019, 10, 13)
 
-diff = now - past
-x = diff / 60 / 60 / 24
-puts "Интервал: #{x} дней"
+def days(past, now)
+  diff = now - past
+  diff / 60 / 60 / 24
+end
+
+def hours(past, now)
+  diff = now - past
+  diff / 60 / 60
+end
+
+def minutes(past, now)
+  diff = now - past
+  diff / 60
+end
+
+puts "Интервал: #{days(past, now).to_i} дней или #{hours(past, now).to_i} часа или #{minutes(past, now).to_i} минут."
