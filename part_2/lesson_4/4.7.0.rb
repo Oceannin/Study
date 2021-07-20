6cdc44c6d949231432af53e532cd3c47070fca53
Vladislav Biketov <vladtheartist@gmail.com>
2021-07-20 21:44:06 +0500

    [new]

--- part_2/lesson_4/4.7.0.rb
+++ part_2/lesson_4/4.7.0.rb
@@ -1,1 +1,14 @@
-# не понял задание, обязательно сделаю его позже 
+class lab
+  @@count = 0
+  @@materials = []
+
+  def perehod 
+    @@count += 1
+    @@materials << Material.new
+    @@materials.last
+  end
+
+  class Material
+# вот здесь надо подумать что происходит с веществом
+  end
+end
