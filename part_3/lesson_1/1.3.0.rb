29d34742917968ac7acac05ecb17fa4d6e404002
Vladislav Biketov <vladtheartist@gmail.com>
2021-08-05 18:28:53 +0500

    [rubocop]

--- part_3/lesson_1/1.3.0.rb
+++ part_3/lesson_1/1.3.0.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 def line
-    File.readlines('for1.txt').minmax.join
+  File.readlines('for1.txt').minmax.join
 end
+
 def longshort
-    File.readlines('for1.txt').minmax.to_s.size 
+  File.readlines('for1.txt').minmax.to_s.size
 end
 
 puts "Слова: #{line}Кол-во символов: #{longshort}"
