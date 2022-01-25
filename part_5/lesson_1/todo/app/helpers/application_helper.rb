4947b181fc5f86edc2660d90142a1152c3bd1d60
Vladislav Biketov <vladtheartist@gmail.com>
2022-01-25 21:52:03 +0500

    [II-3Zadanie]

--- part_5/lesson_1/todo/app/helpers/application_helper.rb
+++ part_5/lesson_1/todo/app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
-end
+    def current_date
+      Date.current.to_s
+    end
+  
+    def action_title(klass, action_name)
+      action_case = action_name == 'index' ? 'many' : 'accusative'
+  
+      [
+        t("label.#{action_name}"),
+        t("activerecord.models.user.#{action_case}")
+      ].join(' ')
+    end
+  
+    def list_tag(arr)
+      tag.ul do
+        safe_join(arr.map { |line| raw tag.li(line) })
+      end
+    end
+  end
+  
